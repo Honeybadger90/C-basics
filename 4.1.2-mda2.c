f0fd0705d2e6fd39b1a554e125d13c131150ea15
Honeybadger90 <101644404+Honeybadger90@users.noreply.github.com>
2022-04-11 11:45:13 -0400

    Add files via upload

---
+++ 4.1.2-mda2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+        int rows = 3;
+    int const columns = 4;
+    int studenGrades[rows][columns] = {0};
+    int studentGrades[rows][columns] = { {1, 3, 4, 6}, {3, 2, 4, 5}, {32, 2, 4, 9}};  
+                             
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+                
+        {
+            printf("%d\t", studentGrades[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
